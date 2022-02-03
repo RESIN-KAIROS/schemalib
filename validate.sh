e70a7211dbd6006a3ae4d5308ccb66c9179c6adf
Zoey Lee <Zoey.Lee13@gmail.com>
2022-02-03 16:26:33 -0600

    add schemas that pass validation.

--- validate.sh
+++ validate.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-SCHEMA_PATH=scenario-schemas/disease_outbreak.json
+SCHEMA_PATH=scenario-schemas
 
-curl -X POST "https://validation.kairos.nextcentury.com/json-ld/ksf/validate" \
-    -H "accept: application/json" \
-    -H "Content-Type: application/ld+json" \
-    -d "$(cat ${SCHEMA_PATH})"
+for schema_file in scenario-schemas/*.json; do
+    echo "Validating ${schema_file}..."
+    curl -X POST "https://validation.kairos.nextcentury.com/json-ld/ksf/validate" \
+        -H "accept: application/json" \
+        -H "Content-Type: application/ld+json" \
+        -d "$(cat ${schema_file})"
+done 
